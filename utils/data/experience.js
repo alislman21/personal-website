5c2ab208561b1b25289baeded82b860909d7b1c4
Abed Alawieh <aalawieh@borninteractive.com>
2024-12-25 17:55:10 +0200

    added the latest experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,13 +1,13 @@
 export const experiences = [
   {
     id: 1,
-    title: "Full Stack Developer",
+    title: "Software Engineer",
     company: "Born Interactive",
     duration: "( May 2024 – Present )",
   },
   {
     id: 2,
-    title: "Full Stack Developer",
+    title: "Software Engineer",
     company: "TDNT (Triplet Data Networks and Telecommunications LTD)",
     duration: "( February 2023 – May 2024 )",
   },
